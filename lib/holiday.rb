0552dc9724dc4e0b22134a1eeb5a7a138ad75159
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2020-02-02 06:45:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, h|
-     puts "#{season.capitalize}:"
+     puts "#{season.to_scapitalize}:"
      holiday.each do |holiday, item|
        puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
      end
