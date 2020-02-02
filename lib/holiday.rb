c99eb46e2fa57448fee49665dfec56cb3da52a3e
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2020-02-02 06:19:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.each do |season, holiday|
     holiday_hash.each do |holiday, array|
-      if array.include
+      if array.include?("BBQ")
 
 end
 
